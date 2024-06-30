743c97fcf53dfeffd51e566de0324a47fff2cde2
Xayrulloh Yusupov <xayrullohyusupov757@gmail.com>
2024-06-30 19:21:11 +0500

    Update git.py

---
+++ git.py
@@ -0,0 +1,9 @@
+""" 
+git init
+git add README.md
+git commit -m "vazifa-6"
+git branch -M main
+git remote add origin https://github.com/xayrullohyusupov/6-oy-vazifa-6.git
+git push -u origin main
+
+"""
